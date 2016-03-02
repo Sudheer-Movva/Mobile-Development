b83e26fed4c34f423400867b03bce3e29bd052f1
Sudheer Movva <smovva1@student.gsu.edu>
2016-03-01 23:53:53 -0500

    Initial Checkin

---
+++ MyFirstProject/src/edu/gsu/cis/ComputeArea.java
@@ -0,0 +1,30 @@
+package edu.gsu.cis;
+
+import java.util.Scanner;
+
+public class ComputeArea {
+	
+	public static void main(String[] args) {
+		
+		//double radius;
+		//double area;
+		
+		//Scanner input = new Scanner(System.in);
+		
+		//System.out.println("Please enter radius");
+		//radius = input.nextDouble();
+		//radius = 10.5;
+		
+		//area = radius*radius*3.14;
+		
+		int a = 10;
+		int b = 20;
+		int c;
+		
+		c = (int) (5/2.0);
+		
+		System.out.println("Value of c "+ c);
+		//System.out.println("Area of a circle " + "with radius "+ radius +" is " + area);
+	}
+
+}
---
+++ MyFirstProject/src/edu/gsu/cis/HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.gsu.cis;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+		
+	System.out.println("Hello World");
+	}
+}
+
